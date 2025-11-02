f850626a458eaf4636950a673e5f5640f1d73ae3
이채은 <chaenn.lee@samsung.com>
2025-11-02 14:38:58 +0900

    update cv

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,24 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "lace",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://example.com/", // replace with your domain
+  author: "Chae Eun Lee",
+  profile: "", // optional personal profile URL
+  desc:
+    "A 3D vision engineer and AI researcher focusing on perception and interaction technologies for XR devices. Passionate about combining computer vision and AI to create intuitive human–computer interactions.",
+  title: "Chae Eun Lee",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
+  postPerIndex: 0,
+  postPerPage: 0,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
-  showArchives: true,
-  showBackButton: true, // show back button in post detail
+  showArchives: false,
+  showBackButton: false,
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "",
   },
-  dynamicOgImage: true,
+  dynamicOgImage: false,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Asia/Seoul", // Default global timezone (IANA format)
 } as const;
